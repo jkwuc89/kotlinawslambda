990517e21511a3f2440b820b84d043df154aec94
Keith Wedinger <keith.wedinger@improving.com>
2018-06-17 10:07:23 -0400

    Refactoring, save and reuse logger instance

--- src/main/kotlin/com/keithwedinger/LambdaRequestHandler.kt
+++ src/main/kotlin/com/keithwedinger/LambdaRequestHandler.kt
@@ -1,6 +1,7 @@
 package com.keithwedinger
 
 import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.LambdaLogger
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.google.gson.Gson
 import com.keithwedinger.tasktracker.data.Task
@@ -16,20 +17,36 @@ import java.time.format.DateTimeFormatter
  */
 class LambdaRequestHandler : RequestHandler<NewTaskRequest, Task> {
     val TASKTRACKER_API_URL = "https://tasktrackerapp.azurewebsites.net/tasktracker-1.0.0-SNAPSHOT/api/tasks"
-    val LOCAL_TASKTRACKER_API_URL = "http://localhost:8080/api/tasks"
+    // val LOCAL_TASKTRACKER_API_URL = "http://localhost:8080/api/tasks"
+    private var logger: LambdaLogger? = null
 
     override fun handleRequest(newTaskRequest: NewTaskRequest, context: Context): Task {
-        context.logger.log("Input: $newTaskRequest")
-        return Task()
+        logger = context.logger
+        logger!!.log("Handling incoming task request. Input: $newTaskRequest")
+        val newTask = createNewTask(newTaskRequest)
+        return postNewTask(newTask)!!
     }
 
+    /**
+     * Create a new Task from the incoming task request
+     */
     fun createNewTask(newTaskRequest: NewTaskRequest): Task {
         val newTaskDueDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
-        val newTask = Task(name = newTaskRequest.name, assignedTo = newTaskRequest.assignedTo, dueDate = newTaskDueDate)
+        return Task(name = newTaskRequest.name, assignedTo = newTaskRequest.assignedTo, dueDate = newTaskDueDate)
+    }
+
+    /**
+     * Post the new task to the web service
+     */
+    private fun postNewTask(newTask: Task): Task? {
         val newTaskPayload = JSONObject(newTask)
         val newTaskResponse = khttp.post(TASKTRACKER_API_URL, json = newTaskPayload)
-        val gson = Gson()
-        val createdTask = gson.fromJson(newTaskResponse.text, Task::class.java)
-        return createdTask
+        return if (newTaskResponse.statusCode == 200) {
+            val gson = Gson()
+            gson.fromJson(newTaskResponse.text, Task::class.java)
+        } else {
+            logger!!.log("*** ERROR ***: Posting new task failed with status code ${newTaskResponse.statusCode}")
+            null
+        }
     }
 }
